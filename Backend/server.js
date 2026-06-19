ebfe4de6691b5ae1fc713f8b3dd81aef165309c5
anjalisharma0909 <bhpndrdhrm@gmail.com>
2026-06-19 06:25:24 -0700

    update code

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+/**
+ * server to start krna 
+ * database ko connect krna 
+ */
+
+require('dotenv').config()
+const app = require('./src/app')
+const connectToDb = require('./src/config/database')
+
+connectToDb()
+
+app.listen(3000,()=>{
+    console.log('server is running on port 3000');
+})
+
+ 
